3eeec358092a9643d1721933b78ddad882673d39
prit <156163564+pritscode@users.noreply.github.com>
2024-03-14 14:15:47 +0900

    Update SearchController.java

--- individualProject/src/main/java/com/gallery/my/search/web/SearchController.java
+++ individualProject/src/main/java/com/gallery/my/search/web/SearchController.java
@@ -1,13 +1,15 @@
 package com.gallery.my.search.web;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class SearchController {
 
 	
 	@RequestMapping("/seoul")
 	public String seoul() {
-		return "/search/seoul";
+		return "search/seoul";
 	}
 	
 }
