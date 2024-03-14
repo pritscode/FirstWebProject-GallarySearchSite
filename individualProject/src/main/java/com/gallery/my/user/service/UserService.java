c75ed9cc694b42442e8982d58b0c41c1aa15bc9f
prit <156163564+pritscode@users.noreply.github.com>
2024-03-14 17:41:07 +0900

    Update UserService.java

--- individualProject/src/main/java/com/gallery/my/user/service/UserService.java
+++ individualProject/src/main/java/com/gallery/my/user/service/UserService.java
@@ -12,6 +12,7 @@ public class UserService {
 	@Autowired
 	IUserDAO dao;
 	
+	// 회원 가입
 	public void registUser(UserVO vo) throws Exception {
 		int result = dao.registUser(vo);
 		if(result == 0) {
@@ -22,6 +23,15 @@ public class UserService {
 	//회원조회
 	public UserVO loginUser(UserVO vo) {
 		return dao.loginUser(vo);
+	}
+	
+	//회원 정보 수정
+	public void modifyingUI(UserVO vo) throws Exception {
+
+		int result = dao.modifyingUI(vo);
+		if( result ==0) {
+			throw new Exception();
+		}
 	}
 	
 }
