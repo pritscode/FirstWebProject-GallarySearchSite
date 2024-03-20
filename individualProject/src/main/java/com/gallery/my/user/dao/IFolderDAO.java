bd754861fa7d11e899e61c8be67535ea6aa699e9
prit <156163564+pritscode@users.noreply.github.com>
2024-03-20 18:47:10 +0900

    Update IFolderDAO.java

--- individualProject/src/main/java/com/gallery/my/user/dao/IFolderDAO.java
+++ individualProject/src/main/java/com/gallery/my/user/dao/IFolderDAO.java
@@ -15,7 +15,13 @@ public interface IFolderDAO {
 	//미술관 폴더 조회
 	public List<FolderVO> userFold(FolderVO vo);
 	//전시회 폴더 조회
-	public List<FolderVO> userserEFold(FolderVO vo);	
+	public List<FolderVO> userEFold(FolderVO vo);
 	//미술관 폴더 내용 조회
 	public List<FolderVO> foldContent(FolderVO vo);
+	//전시회 폴더 내용 조회
+	public List<FolderVO> foldEContent(FolderVO vo);
+	//미술관 폴더 내용 추가
+	public int insertFContent(FolderVO vo);
+	//전시회 폴더 내용 추가
+	public int insertEFContent(FolderVO vo);
 }
