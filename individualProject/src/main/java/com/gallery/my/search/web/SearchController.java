28b1aad504a798bfce19aca30d21a8c611604fd5
prit <156163564+pritscode@users.noreply.github.com>
2024-04-05 17:39:06 +0900

    Update SearchController.java

--- individualProject/src/main/java/com/gallery/my/search/web/SearchController.java
+++ individualProject/src/main/java/com/gallery/my/search/web/SearchController.java
@@ -1,22 +1,47 @@
 package com.gallery.my.search.web;
 
-import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.gallery.my.search.service.SearchService;
+import com.gallery.my.search.vo.SearchVO;
+
 @Controller
 public class SearchController {
 
-	
+	@Autowired
+	SearchService searchservice;
+
 	@RequestMapping("/seoul")
-	public String seoul() {
+	public String seoul(Model model) {
+
+		List<SearchVO> exhList = searchservice.exhList();
+		model.addAttribute("exhList", exhList);
+
 		return "search/seoul";
 	}
-	
+
 	@RequestMapping("/nation")
 	public String nation() {
 		return "search/nation";
 	}
+
+	@RequestMapping("/seoulMuseumofArt")
+	public String seoulMuseumofArt() {
+		return "search/seoulMuseumofArt";
+	}
+
+	@RequestMapping("/eContentView")
+	public String eContentView(Model model, int exhNo) {
+		System.out.println(exhNo);
+		SearchVO exhVO = searchservice.exhCont(exhNo);
+		model.addAttribute("exhVO", exhVO);
+
+		return "search/eContentView";
+	}
+
 }
