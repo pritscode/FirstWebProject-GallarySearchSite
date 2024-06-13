928a9c028c504dce78e9c8f7629029168654aa6f
prit <156163564+pritscode@users.noreply.github.com>
2024-06-13 16:47:04 +0900

    Update HomeController.java

--- exhibitionAssembly/src/main/java/com/gallery/my/HomeController.java
+++ exhibitionAssembly/src/main/java/com/gallery/my/HomeController.java
@@ -2,14 +2,22 @@ package com.gallery.my;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
+
+import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.gallery.my.search.service.SearchService;
+import com.gallery.my.search.vo.exhibitVO;
+import com.gallery.my.search.vo.galleryVO;
 
 /**
  * Handles requests for the application home page.
@@ -19,11 +27,14 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
+	@Autowired
+	SearchService searchService;
+	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public String home(exhibitVO exhvo, HttpSession session, Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
@@ -32,6 +43,15 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
+		
+		
+		List<exhibitVO> popExh = searchService.popExh(exhvo);
+		session.setAttribute("popExh", popExh);
+		System.out.println(popExh);
+		
+		List<exhibitVO> newExh = searchService.newExh(exhvo);
+		session.setAttribute("newExh", newExh);
+		System.out.println(newExh);
 		
 		return "home";
 	}
